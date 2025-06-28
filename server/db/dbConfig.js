d20e7aad7b5aeebe7b29dd262fe3aa8735e2aa95
haimi-gebregiorgis <haimanotwolde1@gmail.com>
2025-06-28 11:47:32 -0400

    Update dbConfig.js

--- server/db/dbConfig.js
+++ server/db/dbConfig.js
@@ -1,20 +1,12 @@
+require("dotenv").config();
 const mysql2 = require("mysql2");
 
-const dbConnection = mysql2.createPool({
-  host: "localhost",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, // MAMP's default MySQL password is 'root'
-  database: process.env.DB_DATABASE, // Replace with your actual DB name
-  port: 8889, // ⚠️ MAMP uses 8889 for MySQL (not 3306)
+const dbconnection = mysql2.createPool({
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  host: process.env.HOST,
+  password: process.env.PASSWORD,
   connectionLimit: 10,
 });
 
-// dbConnection.execute("SELECT 'MAMP Test OK' AS result", (err, results) => {
-//   if (err) {
-//     console.error("MAMP DB connection failed:", err);
-//   } else {
-//     console.log("MAMP DB connection successful:", results);
-//   }
-// });
-
-module.exports = dbConnection.promise();
+module.exports = dbconnection.promise();
